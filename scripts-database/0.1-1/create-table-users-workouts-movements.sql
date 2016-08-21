dc6a2fbfd90d337140e29b0598c60f50c2bddc44
Duy Nguyen <dnguyen0304@gmail.com>
2016-08-21 10:10:12 -0400

    Fixed nullable foreign keys

--- scripts-database/0.1-1/create-table-users-workouts-movements.sql
+++ scripts-database/0.1-1/create-table-users-workouts-movements.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS users_workouts_movements;
 
 CREATE TABLE users_workouts_movements (
 	user_workout_movement_id	serial						PRIMARY KEY,
-	user_workout_id				int							REFERENCES users_workouts (user_workout_id),
-	movement_id					int							REFERENCES movements (movement_id),
+	user_workout_id				int			NOT NULL		REFERENCES users_workouts (user_workout_id),
+	movement_id					int			NOT NULL		REFERENCES movements (movement_id),
 	sets_remaining				smallint	NOT NULL,
 	days_remaining				smallint	NOT NULL,
 	created_on					timestamp	NOT NULL		DEFAULT CURRENT_TIMESTAMP,
