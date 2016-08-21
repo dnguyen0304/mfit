e7407ac1c46ba3f26013d9d3b332068a2e96888d
Duy Nguyen <dnguyen0304@gmail.com>
2016-08-21 08:48:06 -0400

    Fixed not being able to associate records with movements; renamed daily_quota to sets_remaining; renamed weekly_quota to days_remaining

--- scripts-database/0.1-1/create-table-users-workouts-logs.sql
+++ scripts-database/0.1-1/create-table-users-workouts-logs.sql
@@ -1,12 +1,13 @@
-DROP TABLE IF EXISTS users_workouts_logs;
+DROP TABLE IF EXISTS users_workouts_movements;
 
-CREATE TABLE users_workouts_logs (
-	user_workout_log_id		serial						PRIMARY KEY,
-	user_workout_id			int							REFERENCES users_workouts (user_workout_id),
-	daily_quota				smallint	NOT NULL,
-	weekly_quota			smallint	NOT NULL,
-	created_on				timestamp	NOT NULL		DEFAULT CURRENT_TIMESTAMP,
-	created_by				int			NOT NULL,
-	updated_on				timestamp,
-	updated_by				int
+CREATE TABLE users_workouts_movements (
+	user_workout_movement_id	serial						PRIMARY KEY,
+	user_workout_id				int							REFERENCES users_workouts (user_workout_id),
+	movement_id					int							REFERENCES movements (movement_id),
+	sets_remaining				smallint	NOT NULL,
+	days_remaining				smallint	NOT NULL,
+	created_on					timestamp	NOT NULL		DEFAULT CURRENT_TIMESTAMP,
+	created_by					int			NOT NULL,
+	updated_on					timestamp,
+	updated_by					int
 );
