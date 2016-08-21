dc31813e910a54e06e705f9753ffb223a1270312
Duy Nguyen <dnguyen0304@gmail.com>
2016-08-21 08:43:40 -0400

    Fixed not being able to track the number of sets associated with a workout movement

--- scripts-database/0.1-1/create-table-workouts-movements.sql
+++ scripts-database/0.1-1/create-table-workouts-movements.sql
@@ -5,6 +5,7 @@ CREATE TABLE workouts_movements (
 	workout_id					int							REFERENCES workouts (workout_id),
 	movement_id					int							REFERENCES movements (movement_id),
 	sort_order					smallint		NOT NULL	DEFAULT 1,
+	sets						smallint		NOT NULL,
 	value						smallint		NOT NULL,
 	workout_movement_unit_id	smallint					REFERENCES workouts_movements_units (workout_movement_unit_id),
 	created_on					timestamp		NOT NULL	DEFAULT CURRENT_TIMESTAMP,
