c1b0ffb4ab90eac3c93827e2832aa95fbd5c60c9
Duy Nguyen <dnguyen0304@gmail.com>
2016-09-09 16:09:45 -0400

    Fixed circular references

--- mfit_service/__init__.py
+++ mfit_service/__init__.py
@@ -2,8 +2,8 @@
 
 from mfit_service.utilities import configuration
 from mfit_service import models
-from mfit_service import resources
 from mfit_service import services
+from mfit_service import resources
 
 __all__ = ['configuration',
            'models',
