a26e7a4f6d471eeaf64da2e1515a9eaa2a731c79
Duy Nguyen <dnguyen0304@gmail.com>
2016-09-12 16:24:54 -0400

    Removed to_json and _to_json_helper methods

--- mfit_service/models/base.py
+++ mfit_service/models/base.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import collections
-
 from sqlalchemy import Column
 from sqlalchemy.ext.declarative import as_declarative
 
@@ -13,27 +11,4 @@ class Base:
     created_by = Column()
     updated_on = Column()
     updated_by = Column()
-
-    def to_json(self):
-
-        """
-        Return Dictionary
-
-        Convert the object into a JSON representation.
-        """
-
-        # TODO (duyn): ME-201
-        metadata = collections.OrderedDict([
-            ('created_on', str(self.created_on)),
-            ('created_by', str(self.created_by)),
-            ('updated_on', str(self.updated_on)),
-            ('updated_by', str(self.updated_by))])
-
-        document = self._to_json_helper()
-        document.update(metadata)
-
-        return document
-
-    def _to_json_helper(self):
-        pass
 
