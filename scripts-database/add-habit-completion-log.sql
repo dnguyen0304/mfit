bea42d54626d737099e0501c5518ca3e6d8fd730
Duy Nguyen <dnguyen0304@gmail.com>
2016-10-30 12:58:33 -0400

    Migrated to habits data model

--- scripts-database/add-habit-completion-log.sql
+++ scripts-database/add-habit-completion-log.sql
@@ -1,49 +1,48 @@
-CREATE OR REPLACE FUNCTION pg_temp.addMovementCompletionLog (
-	userEmailAddress varchar(64),
-	movementName varchar(32)
+CREATE OR REPLACE FUNCTION pg_temp.addHabitCompletionLog (
+	usersEmailAddress varchar(64),
+	habitsName varchar(32)
 )
 RETURNS void AS
 $$
 BEGIN
-	INSERT INTO users_workouts_movements (
-		user_workout_id,
-		movement_id,
+	INSERT INTO attempts_logs (
+		attempts_id,
+		habits_id,
 		sets_remaining,
-		days_remaining,
 		created_by
 	)
 	(
-		WITH cte AS (
-			-- Get the last log from today for the specified movement.
+		WITH last_logs_today AS (
 			SELECT
-				users_workouts_movements.movement_id,
-				MAX(users_workouts_movements.created_on) AS created_on
-			FROM users_workouts_movements
-			INNER JOIN users_workouts ON users_workouts.user_workout_id = users_workouts_movements.user_workout_id
-			INNER JOIN users ON users.user_id = users_workouts.user_id
-			INNER JOIN movements ON movements.movement_id = users_workouts_movements.movement_id
+				MAX(attempts_logs.created_on) AS created_on,
+				attempts_logs.habits_id
+			FROM attempts_logs
+			INNER JOIN attempts ON attempts.id = attempts_logs.attempts_id
+			INNER JOIN users ON users.id = attempts.users_id
+			INNER JOIN habits ON habits.id = attempts_logs.habits_id
 			WHERE
-				users_workouts.ends_on > CURRENT_TIMESTAMP AND
-				users.email_address = userEmailAddress AND
-				movements.name = movementName AND
-				users_workouts_movements.created_on >= CURRENT_DATE
-			GROUP BY users_workouts_movements.movement_id
+				attempts.starts_at <= CURRENT_TIMESTAMP AND
+				attempts.ends_at > CURRENT_TIMESTAMP AND
+				users.email_address = usersEmailAddress AND
+				habits.name = habitsName AND
+				attempts_logs.created_on >= CURRENT_DATE
+			GROUP BY attempts_logs.habits_id
 		)
 		SELECT
-			users_workouts_movements.user_workout_id,
-			users_workouts_movements.movement_id,
-			users_workouts_movements.sets_remaining - 1 AS sets_remaining,
-			date_part('day', age(users_workouts.ends_on, CURRENT_TIMESTAMP)) AS days_remaining,
-			users_workouts.user_id AS created_by
-		FROM users_workouts_movements
-		INNER JOIN cte ON
-			cte.movement_id = users_workouts_movements.movement_id AND
-			cte.created_on = users_workouts_movements.created_on
-		INNER JOIN users_workouts ON users_workouts.user_workout_id = users_workouts_movements.user_workout_id
-		INNER JOIN users ON users.user_id = users_workouts.user_id
+			attempts_logs.attempts_id,
+			attempts_logs.habits_id,
+			attempts_logs.sets_remaining - 1 AS sets_remaining,
+			attempts.users_id AS created_by
+		FROM attempts_logs
+		INNER JOIN last_logs_today ON
+			last_logs_today.created_on = attempts_logs.created_on AND
+			last_logs_today.habits_id = attempts_logs.habits_id
+		INNER JOIN attempts ON attempts.id = attempts_logs.attempts_id
+		INNER JOIN users ON users.id = attempts.users_id
 		WHERE
-			users_workouts.ends_on > CURRENT_TIMESTAMP AND
-			users.email_address = userEmailAddress
+			attempts.starts_at <= CURRENT_TIMESTAMP AND
+			attempts.ends_at > CURRENT_TIMESTAMP AND
+			users.email_address = usersEmailAddress
 	);
 END
 $$
