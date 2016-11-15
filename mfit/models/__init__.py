6482335a859b7d3c3c44b9ce800a6eadfefc4077
Duy Nguyen <dnguyen0304@gmail.com>
2016-11-15 03:04:22 -0500

    Migrated the Workouts Movements Units model

--- mfit/models/__init__.py
+++ mfit/models/__init__.py
@@ -4,9 +4,11 @@ from mfit.models.base import Base
 from mfit.models.users import Users
 from mfit.models.habit_groups import HabitGroups
 from mfit.models.habits import Habits
+from mfit.models.routines_units import RoutinesUnits
 
 __all__ = ['Base',
            'HabitGroups',
            'Habits',
+           'RoutinesUnits',
            'Users']
 
---
+++ mfit/models/routines_units.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+from sqlalchemy import Column, Integer
+
+from mfit import models
+
+
+class RoutinesUnits(models.Base):
+
+    __tablename__ = 'routines_units'
+
+    id = Column(Integer, primary_key=True)
+    name = Column()
+
+    def __init__(self, name):
+
+        """
+        Routines Units model.
+
+        Parameters
+        ----------
+        name : str
+            Name.
+
+        Attributes
+        ----------
+        id : int
+            Unique identifier.
+        name : str
+            Name.
+        created_at : datetime.datetime
+            When the entity was originally created.
+        created_by : int
+            Who originally created the entity.
+        updated_at : datetime.datetime
+            When the entity was last updated.
+        updated_by : int
+            Who last updated the entity.
+        """
+
+        self.name = name
+
+    def __repr__(self):
+        repr_ = '{}(name="{}")'
+        return repr_.format(self.__class__.__name__, self.name)
+
