11f51abd29974ab7624423343ecb73ea310cb47e
Duy Nguyen <dnguyen0304@gmail.com>
2016-11-15 02:56:16 -0500

    Migrated the Workouts model

--- mfit/models/__init__.py
+++ mfit/models/__init__.py
@@ -2,7 +2,9 @@
 
 from mfit.models.base import Base
 from mfit.models.users import Users
+from mfit.models.habit_groups import HabitGroups
 
 __all__ = ['Base',
+           'HabitGroups',
            'Users']
 
