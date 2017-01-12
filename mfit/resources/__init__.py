d390df90fb08ff8369037bd0c90126abfc16f60b
Duy Nguyen <dnguyen0304@gmail.com>
2017-01-12 15:27:50 -0500

    ME-355: Migrated the Base resource

---
+++ mfit/resources/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from mfit.resources.base import Base
+
+__all__ = ['Base']
+
---
+++ mfit/resources/base.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import flask_restful
+
+import mfit
+from mfit import app
+
+
+class Base(flask_restful.Resource):
+
+    def __init__(self):
+        super().__init__()
+
+        self._db_context_factory = app.DBContextFactory(
+            connection_string=mfit.configuration['repositories']
+                                                ['PostgreSQL']
+                                                ['connection_string'])
+        self._db_context = self._db_context_factory.create()
+
