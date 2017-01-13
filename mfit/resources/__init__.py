ceebf94e3344fc78b95591c0fc7159be171799e2
Duy Nguyen <dnguyen0304@gmail.com>
2017-01-13 18:48:06 -0500

    Revert "ME-355: Migrated Users resource and view"
    This reverts commit e5bfe5a5c5294f3d06d6c65b00ce393343de7d3f.

--- mfit/resources/__init__.py
+++ mfit/resources/__init__.py
@@ -2,9 +2,7 @@
 
 from mfit.resources.base import Base
 from mfit.resources.root import Root
-from mfit.resources.users import Users
 
 __all__ = ['Base',
-           'Root',
-           'Users']
+           'Root']
 
--- mfit/views/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from mfit.views.base import Base
-from mfit.views.users import Users
-
-__all__ = ['Base',
-           'Users']
-
