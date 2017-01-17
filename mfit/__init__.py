dnguyen0304/mfit

commit f0dfcc0fdd25fe95c179e42ee9ce65fdcae93aa9

ME-190: Migrated entry point to mfit/app.py
from mfit/__main__.py; Moved API index from mfit/app.py to resources/__init__.py because of circular dependencies

mfit/app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import datetime
@@ -7,24 +8,11 @@ import flask_restful
 import sqlalchemy
 import sqlalchemy.orm
 
-from mfit import resources
+import mfit
+from mfit import utilities
 
 app = flask.Flask(__name__)
 api = flask_restful.Api(app=app)
-
-api.add_resource(resources.Root, '/v1/')
-api.add_resource(resources.Users, '/v1/users/<int:id>')
-api.add_resource(resources.UsersCollection, '/v1/users/')
-api.add_resource(resources.HabitGroups, '/v1/habit_groups/<int:id>')
-api.add_resource(resources.HabitGroupsCollection, '/v1/habit_groups/')
-api.add_resource(resources.Habits, '/v1/habits/<int:id>')
-api.add_resource(resources.HabitsCollection, '/v1/habits/')
-api.add_resource(resources.Attempts, '/v1/attempts/<int:id>')
-api.add_resource(resources.AttemptsCollection, '/v1/attempts/')
-api.add_resource(resources.Routines, '/v1/routines/<int:id>')
-api.add_resource(resources.RoutinesCollection, '/v1/routines/')
-api.add_resource(resources.AttemptsLogs, '/v1/attempts/<int:attempts_id>/logs/<int:id>')
-api.add_resource(resources.AttemptsLogsCollection, '/v1/attempts/<int:attempts_id>/logs/')
 
 
 class DBContext:
@@ -155,4 +143,11 @@ class DBContextFactory:
         # close the session?
         session = self._SessionFactory()
         return DBContext(session=session)
+
+
+if __name__ == '__main__':
+    if mfit.configuration['environment'] == utilities.Environment.Production.name:
+        app.run()
+    else:
+        app.run(debug=True)
 

mfit/resources/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from mfit import app
 from mfit.resources.base import _Base, Base
 from mfit.resources.base_collection import BaseCollection
 from mfit.resources.root import Root
@@ -31,4 +32,18 @@ __all__ = ['Attempts',
            'RoutinesCollection',
            'Users',
            'UsersCollection']
+
+app.api.add_resource(Root, '/v1/')
+app.api.add_resource(Users, '/v1/users/<int:id>')
+app.api.add_resource(UsersCollection, '/v1/users/')
+app.api.add_resource(HabitGroups, '/v1/habit_groups/<int:id>')
+app.api.add_resource(HabitGroupsCollection, '/v1/habit_groups/')
+app.api.add_resource(Habits, '/v1/habits/<int:id>')
+app.api.add_resource(HabitsCollection, '/v1/habits/')
+app.api.add_resource(Attempts, '/v1/attempts/<int:id>')
+app.api.add_resource(AttemptsCollection, '/v1/attempts/')
+app.api.add_resource(Routines, '/v1/routines/<int:id>')
+app.api.add_resource(RoutinesCollection, '/v1/routines/')
+app.api.add_resource(AttemptsLogs, '/v1/attempts/<int:attempts_id>/logs/<int:id>')
+app.api.add_resource(AttemptsLogsCollection, '/v1/attempts/<int:attempts_id>/logs/')
 

mfit/__main__.py
@@ -1,14 +1,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import mfit
-from mfit import app
-from mfit import utilities
-
-
-if __name__ == '__main__':
-    if mfit.configuration['environment'] == utilities.Environment.Production.name:
-        app.app.run()
-    else:
-        app.app.run(debug=True)
-

commit 331d96e9d58ecbc63f847ff30607ab0d933894a4

Fixed resources not being added
Caused by f0dfcc0fdd25fe95c179e42ee9ce65fdcae93aa9 introducing an incorrect import order; Migrated API index to mfit/__init__.py from resources/__init__.py; Updated documentation

mfit/__init__.py
@@ -1,8 +1,37 @@
 # -*- coding: utf-8 -*-
 
+import flask
+import flask_restful
+
+from mfit import resources
 from mfit import utilities
 
 configuration = utilities.get_configuration(project_name=__name__, depth=1)
 
-__all__ = ['configuration']
+_app = flask.Flask(__name__)
+api = flask_restful.Api(app=_app)
+
+api.add_resource(resources.Root, '/v1/')
+api.add_resource(resources.Users, '/v1/users/<int:id>')
+api.add_resource(resources.UsersCollection, '/v1/users/')
+api.add_resource(resources.HabitGroups, '/v1/habit_groups/<int:id>')
+api.add_resource(resources.HabitGroupsCollection, '/v1/habit_groups/')
+api.add_resource(resources.Habits, '/v1/habits/<int:id>')
+api.add_resource(resources.HabitsCollection, '/v1/habits/')
+api.add_resource(resources.Attempts, '/v1/attempts/<int:id>')
+api.add_resource(resources.AttemptsCollection, '/v1/attempts/')
+api.add_resource(resources.Routines, '/v1/routines/<int:id>')
+api.add_resource(resources.RoutinesCollection, '/v1/routines/')
+api.add_resource(resources.AttemptsLogs, '/v1/attempts/<int:attempts_id>/logs/<int:id>')
+api.add_resource(resources.AttemptsLogsCollection, '/v1/attempts/<int:attempts_id>/logs/')
+
+
+def main():
+    if configuration['environment'] == utilities.Environment.Production.name:
+        _app.run()
+    else:
+        _app.run(debug=True)
+
+
+__all__ = ['api', 'configuration', 'main']
 

mfit/app.py
@@ -3,16 +3,10 @@
 
 import datetime
 
-import flask
-import flask_restful
 import sqlalchemy
 import sqlalchemy.orm
 
 import mfit
-from mfit import utilities
-
-app = flask.Flask(__name__)
-api = flask_restful.Api(app=app)
 
 
 class DBContext:
@@ -146,8 +140,5 @@ class DBContextFactory:
 
 
 if __name__ == '__main__':
-    if mfit.configuration['environment'] == utilities.Environment.Production.name:
-        app.run()
-    else:
-        app.run(debug=True)
+    mfit.main()
 

mfit/resources/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from mfit import app
 from mfit.resources.base import _Base, Base
 from mfit.resources.base_collection import BaseCollection
 from mfit.resources.root import Root
@@ -32,18 +31,4 @@ __all__ = ['Attempts',
            'RoutinesCollection',
            'Users',
            'UsersCollection']
-
-app.api.add_resource(Root, '/v1/')
-app.api.add_resource(Users, '/v1/users/<int:id>')
-app.api.add_resource(UsersCollection, '/v1/users/')
-app.api.add_resource(HabitGroups, '/v1/habit_groups/<int:id>')
-app.api.add_resource(HabitGroupsCollection, '/v1/habit_groups/')
-app.api.add_resource(Habits, '/v1/habits/<int:id>')
-app.api.add_resource(HabitsCollection, '/v1/habits/')
-app.api.add_resource(Attempts, '/v1/attempts/<int:id>')
-app.api.add_resource(AttemptsCollection, '/v1/attempts/')
-app.api.add_resource(Routines, '/v1/routines/<int:id>')
-app.api.add_resource(RoutinesCollection, '/v1/routines/')
-app.api.add_resource(AttemptsLogs, '/v1/attempts/<int:attempts_id>/logs/<int:id>')
-app.api.add_resource(AttemptsLogsCollection, '/v1/attempts/<int:attempts_id>/logs/')
 

mfit/resources/base.py
@@ -83,7 +83,7 @@ class Base(_Base):
 
     @classmethod
     def get_self_link(cls, entity):
-        return app.api.url_for(cls._resource, id=entity.id, _external=True)
+        return mfit.api.url_for(cls._resource, id=entity.id, _external=True)
 
     @classmethod
     def to_json(cls, entity):

mfit/resources/base_collection.py
@@ -4,7 +4,7 @@ import collections
 
 import flask
 
-from mfit import app
+import mfit
 from mfit import resources
 
 
@@ -20,7 +20,7 @@ class BaseCollection(resources._Base):
                 in self._db_context.query(self._model).all()]
 
         links = {
-            'self': app.api.url_for(self._resource_collection, _external=True)
+            'self': mfit.api.url_for(self._resource_collection, _external=True)
         }
 
         return collections.OrderedDict([('data', data), ('links', links)])

mfit/resources/root.py
@@ -2,7 +2,7 @@
 
 import collections
 
-from mfit import app
+import mfit
 from mfit import resources
 
 
@@ -11,24 +11,24 @@ class Root(resources._Base):
     def get(self):
         data = [
             {
-                'attempts': app.api.url_for(resources.AttemptsCollection, _external=True)
+                'attempts': mfit.api.url_for(resources.AttemptsCollection, _external=True)
             },
             {
-                'habit_groups': app.api.url_for(resources.HabitGroupsCollection, _external=True)
+                'habit_groups': mfit.api.url_for(resources.HabitGroupsCollection, _external=True)
             },
             {
-                'habits': app.api.url_for(resources.HabitsCollection, _external=True)
+                'habits': mfit.api.url_for(resources.HabitsCollection, _external=True)
             },
             {
-                'routines': app.api.url_for(resources.RoutinesCollection, _external=True)
+                'routines': mfit.api.url_for(resources.RoutinesCollection, _external=True)
             },
             {
-                'users': app.api.url_for(resources.UsersCollection, _external=True)
+                'users': mfit.api.url_for(resources.UsersCollection, _external=True)
             }
         ]
 
         links = {
-            'self': app.api.url_for(Root, _external=True)
+            'self': mfit.api.url_for(Root, _external=True)
         }
 
         return collections.OrderedDict([('data', data), ('links', links)])