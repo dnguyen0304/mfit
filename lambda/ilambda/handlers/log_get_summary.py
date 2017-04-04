6341f7022a3b1e5ffc7f1fc2d4186c1224d67c1d
ubuntu <dnguyen0304@gmail.com>
2017-04-04 06:14:46 -0400

    ME-621: Added Lambda handler for getting log summaries.

---
+++ lambda/ilambda/handlers/log_get_summary.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function
+
+import site
+
+site.addsitedir('.')
+
+import mfit
+
+
+def log_get_summary(event, context):
+
+    results = mfit.get_all_from_today()
+    for result in results:
+        print(result)
